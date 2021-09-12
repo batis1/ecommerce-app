bb567e617cc2b0a3d88874cf7dab9cbfbd03b4f8
batis <batis2019@gmail.com>
2021-09-12 15:56:41 +0800

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input placeholder="Search" />
+            <Input placeholder="saerch" />
             <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
